117c735b46c8d9f54a1956366efb5a8e67e88ff7
AndrushaK <Kotchuruba@gmail.com>
2022-09-09 15:54:43 +0300

    Initialize Project

---
+++ app/webpack/packs/application.js
@@ -0,0 +1,12 @@
+
+import Rails from '@rails/ujs'
+import Turbolinks from 'turbolinks'
+Rails.start()
+Turbolinks.start()
+
+import $ from 'jquery';
+
+window.$ = $;
+window.jQuery = $;
+
+import 'bootstrap-sass/assets/javascripts/bootstrap';
